82814889868102b7006716944c2db69539dd0f24
Maulik Chhabra <maulikchhabra@gmail.com>
2020-07-04 23:47:59 +0530

    Category work

--- WebContent/js/beginner.js
+++ WebContent/js/beginner.js
@@ -50,3 +50,45 @@ function showBiceps(){
 }
 }
     
+function showTriceps(){
+    var click=document.getElementById("drop-content5");
+    var a1=document.getElementById("drop-content2");
+    var a2=document.getElementById("drop-content3");
+    var a3=document.getElementById("drop-content1");
+    var a4=document.getElementById("drop-content4");
+    if(a1.style.display==="none" && a2.style.display==="none" && a3.style.display==="none" && a4.style.display==="none"){
+        if(click.style.display==="none"){
+            click.style.display="block";
+        }
+        else{click.style.display="none";}
+}
+}
+    
+function showAbs(){
+    var click=document.getElementById("drop-content6");
+    var a1=document.getElementById("drop-content2");
+    var a2=document.getElementById("drop-content3");
+    var a3=document.getElementById("drop-content1");
+    var a4=document.getElementById("drop-content4");
+    var a5=document.getElementById("drop-content5");
+    if(a1.style.display==="none" && a2.style.display==="none" && a3.style.display==="none" && a4.style.display==="none" && a5.style.display==="none"){
+        if(click.style.display==="none"){
+            click.style.display="block";
+        }
+        else{click.style.display="none";}
+}
+}
+    
+function showLegs(){
+    var click=document.getElementById("drop-content4");
+    var a1=document.getElementById("drop-content2");
+    var a2=document.getElementById("drop-content3");
+    var a3=document.getElementById("drop-content1");
+    if(a1.style.display==="none" && a2.style.display==="none" && a3.style.display==="none"){
+        if(click.style.display==="none"){
+            click.style.display="block";
+        }
+        else{click.style.display="none";}
+}
+}
+    
